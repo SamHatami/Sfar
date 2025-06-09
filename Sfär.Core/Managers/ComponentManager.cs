d4da02b743bb0c7e48506180b632cafbad99a974
Sam Hatami <mhatami@hotmail.com>
2025-06-09 13:09:08 +0200

    Update ComponentManager.cs
    Component Manager now reads component types directly from namespace and adds the types automatically if it is an IComponent.
    
    Removed static behavior

--- Sfär.Core/Managers/ComponentManager.cs
+++ Sfär.Core/Managers/ComponentManager.cs
@@ -1,5 +1,63 @@
 ﻿namespace Sfär.Core.Managers;
 
-public static class ComponentManager
+public class ComponentManager
 {
+
+      private Dictionary<int,List<int>> ComponentMap = new(); 
+
+      private Dictionary<Type, int> ComponentIds = new();
+    
+    
+    //alternativt en Dictionary<int,ulong> där uLong får bit-operation. Men för att få ut index för varje entitet så behövs en loop genom ulong oavsett?
+    public ComponentManager(){
+  
+      var componentTypes = from t in Assembly.GetExecutingAssembly().GetTypes()
+            where t.IsValueType && !t.IsEnum && t.Namespace == "Sfär.Core.Components"
+            select t;
+        
+        for (var i = 0; i < componentTypes.Count(); i++)
+        {
+            try
+            {
+                var componentType = componentsList[i];
+                
+                if (typeof(IComponent).IsAssignableFrom(componentType))
+                  ComponentIds[componentType] = i; 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Could not add component {components.ElementAt(i).Name} to Components");
+            }
+        }
+      
+    }
+    //go through assembly and namespace and add to this dictionary instead.....
+
+    
+
+    public int GetId<T>()
+    {
+        return ComponentIds[typeof(T)];
+    }
+
+    public void AddToComponentMap<T>(int entityId)
+    {
+        var componentId = GetId<T>();
+        
+        if (!ComponentMap.ContainsKey(componentId))
+            ComponentMap.Add(componentId, new List<int>());
+        
+        var componentMask = ComponentMap[componentId];
+        componentMask.Add(entityId);
+    }
+
+    public int[] GetEntityIdsFor<T>()
+    {
+        var componentId = GetId<T>();
+        var componentMask = ComponentMap[componentId];
+       
+        return componentMask.ToArray();
+    }
+
+
 }
